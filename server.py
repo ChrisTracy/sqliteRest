7dc2f337f02595472813325aaa787ddd500ed9db
Chris Tracy <58871574+ChrisTracy@users.noreply.github.com>
2023-12-07 20:26:53 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from typing import Optional, Dict
 from fastapi import Request
 import aiosqlite
@@ -30,35 +30,40 @@ if not os.path.exists(database_path):
     sys.exit(1)
 
 async def dynamic_query(table_name, conditions, limit=return_item_limit, all_fields=False):
-    async with aiosqlite.connect(database_path) as conn:
-        cursor = await conn.cursor()
+    try:
+        async with aiosqlite.connect(database_path) as conn:
+            cursor = await conn.cursor()
 
-        query_conditions = []
-        values = []
+            query_conditions = []
+            values = []
 
-        for column, value in conditions.items():
-            if '*' in value:
-                query_conditions.append(f"LOWER({column}) LIKE ?")
-                values.append(f'%{value.replace("*", "").lower()}%')
+            for column, value in conditions.items():
+                if '*' in value:
+                    query_conditions.append(f"LOWER({column}) LIKE ?")
+                    values.append(f'%{value.replace("*", "").lower()}%')
+                else:
+                    query_conditions.append(f"LOWER({column}) = ?")
+                    values.append(value.lower())
+
+            query = f"SELECT * FROM {table_name} WHERE {' AND '.join(query_conditions)}"
+            query += f" LIMIT ?"
+
+            # Use parameterized query to prevent SQL injection
+            await cursor.execute(query, (*values, limit))
+
+            columns = [desc[0] for desc in cursor.description]
+            result = await cursor.fetchall()
+
+            if all_fields:
+                result_as_json = [dict(zip(columns, row)) for row in result]
             else:
-                query_conditions.append(f"LOWER({column}) = ?")
-                values.append(value.lower())
+                result_as_json = [dict((k, v) for k, v in zip(columns, row) if v) for row in result]
 
-        query = f"SELECT * FROM {table_name} WHERE {' AND '.join(query_conditions)}"
-        query += f" LIMIT ?"
+        return {"status_code": 200, "message": "ok", "data": result_as_json}
 
-        # Use parameterized query to prevent SQL injection
-        await cursor.execute(query, (*values, limit))
-
-        columns = [desc[0] for desc in cursor.description]
-        result = await cursor.fetchall()
-
-        if all_fields:
-            result_as_json = [dict(zip(columns, row)) for row in result]
-        else:
-            result_as_json = [dict((k, v) for k, v in zip(columns, row) if v) for row in result]
-
-    return result_as_json
+    except Exception as e:
+        logging.error(f"Error processing query for table '{table_name}' with conditions {conditions}: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.get("/api/{table_name}")
 async def read_item(table_name: str, request: Request):
@@ -67,7 +72,8 @@ async def read_item(table_name: str, request: Request):
         all_fields = params.pop("all_fields", False)  # Remove and get the value of all_fields
         result = await dynamic_query(table_name, params, all_fields=all_fields)
         logging.info(f"Query successful for table '{table_name}' with conditions {params}")
-        return result
-    except Exception as e:
-        logging.error(f"Error processing query for table '{table_name}' with conditions {params}: {str(e)}")
-        raise HTTPException(status_code=500, detail=str(e))
+        return Response(content=result, media_type="application/json")
+
+    except HTTPException as http_exc:
+        # If HTTPException is raised, reformat the response
+        return Response(content={"status_code": http_exc.status_code, "message": http_exc.detail}, media_type="application/json")
